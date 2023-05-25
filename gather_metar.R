3262b0e624c81fdc941c53e36b5290a938278303
Freyja Brandel-Tanis <30274062+freyja-bt@users.noreply.github.com>
2023-05-25 12:17:41 +0200

    update loading package structure

--- gather_metar.R
+++ gather_metar.R
@@ -1,5 +1,4 @@
-source("R/metar.R")
-new_entries <- read_metar(stations = c("KPDK","KATL"), hours=120)%>%
-  recent_metar(path = "raw_data/metar_runs.csv")
+
+new_entries <- rMETAR::recent_metar(rMETAR::read_metar(stations = c("KPDK","KATL"), hours=120), path = "raw_data/metar_runs.csv")
 
 readr::write_csv(new_entries, "raw_data/metar_runs.csv", append = T)
